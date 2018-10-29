03076ccd251751dfc1a48c005bf2673eabc89eb1
Stig Sandbeck Mathisen <stig.mathisen@soprasteria.com>
2018-10-29 10:39:49 +0100

    molecule: Add preparation and tests

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,12 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_package(host):
+    p = host.package("icingaweb2")
+    assert p.is_installed
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_service(host):
+    s = host.service('icingaweb2')
+    assert s.is_running
+    assert s.is_enabled
